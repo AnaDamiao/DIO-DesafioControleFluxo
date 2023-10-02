987f6ab1f2a14b6cabb9e0f1fb97e8732394edb2
Ana Damiao <ana.damiao@iob.com.br>
2023-10-02 18:05:51 -0300

    Criando uma contadora no desafio de controle de fluxo

---
+++ README.md
@@ -0,0 +1,3 @@
+## DesafioControleFluxo
+
+Este é um projeto Java simples que permite ao usuário fornecer dois números inteiros e, em seguida, imprime uma contagem a partir do primeiro número até o segundo número (ou seja, a subtração do segundo número pelo primeiro). Se o primeiro número for maior que o segundo, ele lançará uma exceção personalizada ParametrosInvalidosException.
---
+++ src/Contador.java
@@ -0,0 +1,35 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) throws Exception {
+        Scanner terminal = new Scanner(System.in).useLocale(Locale.US);
+       
+        System.out.println("Informe o primeiro número: ");
+        int numero1 = terminal.nextInt();
+        System.out.println("Informe o segundo número: ");
+        int numero2 = terminal.nextInt();
+        
+        try {
+            contar(numero1, numero2);
+        } catch (ParametrosInvalidosException e) {
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro.");
+        }
+    }
+    static void contar(int numero1, int numero2) throws ParametrosInvalidosException {
+        if (numero1 > numero2) {
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior ou igual ao primeiro.");
+        }
+
+        int contagem = numero2 - numero1;
+        for (int index = 1; index <= contagem; index++) {
+            System.out.println("Imprimindo o número " + index);
+        }
+    }
+}
+//Classe exceção
+class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String mensagemExecao) {
+        super(mensagemExecao);
+    }
+}
